87f2a911d93041ab6fd9c944ca6a7b4da452469a
lucas4lves <zombnfl@gmail.com>
2025-03-25 14:44:01 -0300

    Chore: removing comments from stack file

--- lib/cdk-aws-ts-stack.ts
+++ lib/cdk-aws-ts-stack.ts
@@ -1,16 +1,9 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkAwsTsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkAwsTsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    
   }
 }
